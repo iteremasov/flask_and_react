aeb7a7c34a03bb1aed0546c38dc083c8be21b9c1
iteremasov <teremasovjr@gmail.com>
2020-10-31 19:10:27 +0600

    test commit for test_branch

--- backend/config.py
+++ backend/config.py
@@ -7,3 +7,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# this is a chenge string for test_branch ))))))
